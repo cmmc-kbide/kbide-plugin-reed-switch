ef116cc59f96666574492d33c4969894c9fb476c
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 14:00:21 +0700

    add generator

--- blocks/blocks_reed-switch.js
+++ blocks/blocks_reed-switch.js
@@ -1,18 +1,32 @@
-Blockly.Blocks['ky_025_reed_switch_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-025 REED SWITCH");
-    this.appendValueInput("KY_025_REED_SWITCH_ANALOG_PIN")
-        .setCheck("Number")
-        .appendField("ANALOG PIN");
-    this.appendValueInput("KY_025_REED_SWITCH_DIGITAL_PIN")
-        .setCheck("Number")
-        .appendField("DIGITAL PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_025_reed_switch_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-025 REED SWITCH");
+      this.appendDummyInput()
+        .appendField("ANALOG PIN")
+        .appendField(
+          new Blockly.FieldTextInput("A0"),
+          "KY_025_REED_SWITCH_ANALOG_PIN"
+        )
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_025_REED_SWITCH_ANALOG_VALUE"),
+          "KY_025_REED_SWITCH_ANALOG_VALUE"
+        );
+      this.appendDummyInput()
+        .appendField("DIGITAL PIN")
+        .appendField(
+          new Blockly.FieldTextInput("0"),
+          "KY_025_REED_SWITCH_DIGITAL_PIN"
+        )
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_025_REED_SWITCH_DIGITAL_VALUE"),
+          "KY_025_REED_SWITCH_DIGITAL_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_reed-switch.js
+++ blocks/generators_reed-switch.js
@@ -1,7 +1,36 @@
-Blockly.JavaScript['ky_025_reed_switch_block'] = function(block) {
-  var value_ky_025_reed_switch_analog_pin = Blockly.JavaScript.valueToCode(block, 'KY_025_REED_SWITCH_ANALOG_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_ky_025_reed_switch_digital_pin = Blockly.JavaScript.valueToCode(block, 'KY_025_REED_SWITCH_DIGITAL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_025_reed_switch_block"] = function(block) {
+    var text_ky_025_reed_switch_analog_pin = block.getFieldValue(
+      "KY_025_REED_SWITCH_ANALOG_PIN"
+    );
+    var variable_ky_025_reed_switch_analog_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_025_REED_SWITCH_ANALOG_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var text_ky_025_reed_switch_digital_pin = block.getFieldValue(
+      "KY_025_REED_SWITCH_DIGITAL_PIN"
+    );
+    var variable_ky_025_reed_switch_digital_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_025_REED_SWITCH_DIGITAL_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	
+	#VARIABLE
+	int KY_025_REED_SWITCH_DIGITAL_PIN = ${text_ky_025_reed_switch_digital_pin}; // KY-025 digital interface
+	int KY_025_REED_SWITCH_ANALOG_PIN = ${text_ky_025_reed_switch_analog_pin}; // KY-025 analog interface
+	#END
+
+	#SETUP
+	  pinMode(KY_025_REED_SWITCH_DIGITAL_PIN, INPUT);
+	  pinMode(KY_025_REED_SWITCH_ANALOG_PIN, OUTPUT);
+	#END
+
+	// Read the digital interface
+  	${variable_ky_025_reed_switch_digital_value} = digitalRead(KY_025_REED_SWITCH_DIGITAL_PIN); 
+  	// Read the analog interface
+  	${variable_ky_025_reed_switch_analog_value} = analogRead(KY_025_REED_SWITCH_ANALOG_PIN); 
+  	delay(100);
+    `;
+    return code;
+  };
